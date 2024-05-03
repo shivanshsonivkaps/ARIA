ee927973faa0638b67a2ae1b8450d8b5dd82a5cc
Shivansh <shivansh.soni@vkaps.com>
2024-05-03 20:19:51 +0530

    fixed error:

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -89,17 +89,17 @@ export async function POST(req: Request) {
       name: fullName,
     };
 
-    const updatedUser = await updateUser(id, user);
+    // const updatedUser = await updateUser(id, user);
 
-    return NextResponse.json({ message: "OK", user: updatedUser });
+    // return NextResponse.json({ message: "OK", user: updatedUser });
   }
 
   if (eventType === "user.deleted") {
     const { id } = evt.data;
 
-    const deletedUser = await deleteUser(id!);
+    // const deletedUser = await deleteUser(id!);
 
-    return NextResponse.json({ message: "OK", user: deletedUser });
+    // return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
   return new Response("", { status: 200 });
